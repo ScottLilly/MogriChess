105374471f8fe95d76de718cf2113b04a1c34003
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2021-11-26 16:52:06 -0600

    Created PieceFactory

---
+++ MogriChess.Models/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace MogriChess.Models
+{
+    public static class Constants
+    {
+        public const int NumberOfFiles = 8;
+        public const int NumberOfRanks = 8;
+
+        public const int UnlimitedMoves = 99;
+
+        public const int BackRankDark = 8;
+        public const int PawnRankDark = 7;
+        public const int PawnRankLight = 2;
+        public const int BackRankLight = 1;
+    }
+}
---
+++ MogriChess.Services/PieceFactory.cs
@@ -0,0 +1,51 @@
+﻿using MogriChess.Models;
+
+using Constants = MogriChess.Models.Constants;
+
+namespace MogriChess.Services
+{
+    public static class PieceFactory
+    {
+        internal static Piece GetBishop(Enums.PlayerColor color)
+        {
+            return new Piece(color, Enums.PieceType.Other,
+                0, Constants.UnlimitedMoves,
+                0, Constants.UnlimitedMoves,
+                0, Constants.UnlimitedMoves,
+                0, Constants.UnlimitedMoves);
+        }
+
+        internal static Piece GetKing(Enums.PlayerColor color)
+        {
+            return new Piece(color, Enums.PieceType.King, 1, 1, 1, 1, 1, 1, 1, 1);
+        }
+
+        internal static Piece GetKnight(Enums.PlayerColor color)
+        {
+            return new Piece(color, Enums.PieceType.Other, 2, 2, 2, 2, 2, 2, 2, 2);
+        }
+
+        internal static Piece GetPawn(Enums.PlayerColor color)
+        {
+            return new Piece(color, Enums.PieceType.Pawn, 1, 1, 0, 0, 0, 0, 0, 1);
+        }
+
+        internal static Piece GetQueen(Enums.PlayerColor color)
+        {
+            return new Piece(color, Enums.PieceType.Other,
+                Constants.UnlimitedMoves, Constants.UnlimitedMoves,
+                Constants.UnlimitedMoves, Constants.UnlimitedMoves,
+                Constants.UnlimitedMoves, Constants.UnlimitedMoves,
+                Constants.UnlimitedMoves, Constants.UnlimitedMoves);
+        }
+
+        internal static Piece GetRook(Enums.PlayerColor color)
+        {
+            return new Piece(color, Enums.PieceType.Other,
+                Constants.UnlimitedMoves, 0,
+                Constants.UnlimitedMoves, 0,
+                Constants.UnlimitedMoves, 0,
+                Constants.UnlimitedMoves, 0);
+        }
+    }
+}
