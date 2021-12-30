b36800f5f9f0cbcab794aedd364914d5fb56bdf2
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2021-12-30 14:11:23 -0600

    Refactoring checkpoint

--- MogriChess.Models/Move.cs
+++ MogriChess.Models/Move.cs
@@ -4,20 +4,18 @@
     {
         public Enums.ColorType MovingPieceColor { get; }
         public Square FromSquare { get; }
-        public int FromRank => FromSquare.Rank;
-        public int FromFile => FromSquare.File;
-        public string FromFileAsLetter => FromSquare.FileAsLetter;
         public Square DestinationSquare { get; }
+
         public int DestinationRank => DestinationSquare.Rank;
         public int DestinationFile => DestinationSquare.File;
-        public string DestinationFileAsLetter => DestinationSquare.FileAsLetter;
+
         public bool IsCheckmateMove { get; set; }
         public bool IsCheckMove { get; set; }
         public bool IsCapturingMove { get; set; }
         public bool IsPromotingMove { get; set; }
 
         public string MoveShorthand =>
-            $"{FromFileAsLetter}{FromRank}:{DestinationFileAsLetter}{DestinationRank}";
+            $"{FromSquare.SquareShorthand}:{DestinationSquare.SquareShorthand}";
 
         public string MoveResult =>
             IsCheckmateMove ? "Checkmate" :
