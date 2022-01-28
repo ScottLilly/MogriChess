ab0310b0249cb968e3e4ea2a07365463b01572bd
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-01-28 16:27:35 -0600

    Cleanup

--- MogriChess.Models/DTOs/GameStateDTO.cs
+++ MogriChess.Models/DTOs/GameStateDTO.cs
@@ -7,7 +7,8 @@ public class GameStateDTO
     public ColorScheme BoardColorScheme { get; set; }
     public ColorScheme PieceColorScheme { get; set; }
     public Enums.Color CurrentPlayerColor { get; set; }
-    public List<MoveHistoryDTO> MoveHistory { get; set; }
+    public List<MoveHistoryDTO> MoveHistory { get; set; } =
+        new List<MoveHistoryDTO>();
     public List<Square> Squares { get; set; } =
         new List<Square>();
 }
