d720aa231cc4288f40166f690228ddc0bac446ad
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-02-09 16:05:16 -0600

    Fixed Save Game Board State bug

---
+++ Test.MogriChess.ViewModels/TestGameStateService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MogriChess.Models;
+using MogriChess.Models.DTOs;
+using MogriChess.Services;
+using MogriChess.ViewModels;
+using Test.MogriChess.ViewModels.Utilities;
+using Xunit;
+
+
+namespace Test.MogriChess.ViewModels
+{
+    public class TestGameStateService
+    {
+        public void Test_GetSerializedGameState()
+        {
+            string json =
+                File.ReadAllText(".\\GameStateFiles\\StartOfGame_GameState.json");
+
+            PlaySession session = new PlaySession();
+            session.StartGame(Enums.PlayerType.Human, Enums.PlayerType.Bot);
+
+            //Assert.Equal(json, session.GetSerializedGameState());
+        }
+    }
+}
